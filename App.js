4733ebb77f1b4860269651845bcba65e5c2c4c33
Dominik Skalník <dominik.skalnik@heidelbergcement.com>
2019-06-19 16:01:20 +0200

    Bug repro demo.

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    items: [{ key: 'a' }, { key: 'b' }, { key: 'c' }],
+    activeItem: 0,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      const { items, activeItem: oldActiveItem } = this.state;
+      const activeItem = (oldActiveItem < items.length - 1) ? oldActiveItem + 1 : 0;
+
+      this.setState({ activeItem });
+    }, 1500);
+  }
+
   render() {
+    const { items, activeItem } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.statusBar} />
+        <Text>Correct behavior: re-renders with extraData prop</Text>
+        <FlatList
+          data={items}
+          extraData={activeItem}
+          renderItem={({ item, index }) => {
+            const isActive = index === activeItem;
+            const style = isActive ? { backgroundColor: 'red' } : {};
+            return <Text style={style}>{item.key}</Text>;
+          }}
+        />
+        <Text>Correct behavior: does not re-render without extraData prop</Text>
+        <FlatList
+          data={items}
+          renderItem={({ item, index }) => {
+            const isActive = index === activeItem;
+            const style = isActive ? { backgroundColor: 'red' } : {};
+            return <Text style={style}>{item.key}</Text>;
+          }}
+        />
+        <Text>Incorrect behavior: re-renders without extraData prop (with style property)</Text>
+        <FlatList
+          data={items}
+          style={{margin: 5}}
+          renderItem={({ item, index }) => {
+            const isActive = index === activeItem;
+            const style = isActive ? { backgroundColor: 'red' } : {};
+            return <Text style={style}>{item.key}</Text>;
+          }}
+        />
       </View>
     );
   }
@@ -17,5 +63,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  statusBar: {
+    backgroundColor: '#C2185B',
+    height: Constants.statusBarHeight,
   },
 });
